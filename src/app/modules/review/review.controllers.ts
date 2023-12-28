1db47dc12090150a2ccb41475d7130f03a98f4b3
Tausif Ahmed <tausif.ritu1@gmail.com>
2023-12-29 01:01:41 +0600

    Create a Review (Only the user can do this)

--- src/app/modules/review/review.controllers.ts
+++ src/app/modules/review/review.controllers.ts
@@ -1,8 +1,16 @@
 import { RequestHandler } from 'express';
 import { ReviewServices } from './review.services';
-
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 const createReview: RequestHandler = async (req, res) => {
   try {
+    const accessToken = req.headers.authorization;
+
+    const decoded = jwt.verify(
+      accessToken as string,
+      config.jwt_secret as string,
+    );
+    req.body.createdBy = decoded._id;
     const data = await ReviewServices.createReviewService(req.body);
     res.status(201).json({
       success: true,
--- src/app/modules/review/review.routes.ts
+++ src/app/modules/review/review.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { ReviewControllers } from './review.controllers';
 import zodValidation from '../../middlewares/validations/zodValidations';
 import { ReviewZodValidationSchema } from './review.validation.zod';
+import checkAuth from '../../middlewares/checkAuth';
 
 const router = Router();
 router.post(
   '/',
   zodValidation(ReviewZodValidationSchema.createReviewSchema),
+  checkAuth('user'),
   ReviewControllers.createReview,
 );
 
